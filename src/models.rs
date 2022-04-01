c30cf9d90e98b485725aacc3371982408c50a704
Distopico Vegan <distopico@riseup.net>
2022-04-01 07:41:42 -0500

    feat: integration with user api

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,10 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct User {
-    pub id: i32,
-    pub name: String,
+    // pub id: i32, TODO: missing user id from user service
+    pub username: String,
+    pub enabled: bool,
+    pub account_non_locked: bool,
 }
